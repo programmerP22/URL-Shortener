64435caff869c4cca0cb51f538493c6f1c96a5ef
programmerP22 <chiangcw19@gmail.com>
2022-10-18 22:48:13 +0800

    feat: update models

---
+++ models/URL.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const URLSchema = new Schema({
+  originalURL: {
+    type: String, 
+    required: true 
+  },
+  shortenedURL: {
+    type: String,
+    required: true 
+  }
+})
+module.exports = mongoose.model('URL', URLSchema)
